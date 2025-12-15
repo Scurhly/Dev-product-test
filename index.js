76a2aa4ecf2859813d6db1e02852c986cf44b105
Scurhly <rafaelong200208@gmail.com>
2025-12-16 00:40:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
- 
-
-//as
-
-import express from "express";
-import fetch from "node-fetch";
-import dotenv from "dotenv";
-
-dotenv.config();
+const express = require("express");
+const fetch = require("node-fetch");
+require("dotenv").config();
 
 const app = express();
 app.use(express.json());
@@ -20,40 +14,30 @@ app.post("/test", (req, res) => {
   res.json({ ok: true });
 });
 
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
 });
 
 app.post("/create-product", async (req, res) => {
-    const { amount, userId } = req.body;
-  
-    if (!Number.isInteger(amount) || amount < 1 || amount > 20000) {
-      return res.status(400).json({ error: "Invalid amount" });
-    }
-  
- 
-    const PRODUCT_MAP = {
-      1: 3480326033,
-      5: 3480326507,
-      10: 3480326513,
-      50: 3480326517,
-      100: 3480327484,
-      1000: 3480327494,
-      2000: 3480327507,
-      5000: 3480327517,
-    };
-  
-    const productId = PRODUCT_MAP[amount];
-    if (!productId) {
-      return res.status(400).json({ error: "Unsupported amount" });
-    }
-  
-    res.json({ productId });
-  });
-  
+  const { amount, userId } = req.body;
 
-
-//api
+  if (!Number.isInteger(amount) || amount < 1 || amount > 20000) {
+    return res.status(400).json({ error: "Invalid amount" });
+  }
 
  
+  const PRODUCT_MAP = {
+    1: 111111,
+    5: 222222,
+    10: 333333,
+    50: 444444,
+    100: 555555
+  };
+
+  const productId = PRODUCT_MAP[amount];
+  if (!productId) {
+    return res.status(400).json({ error: "Unsupported amount" });
+  }
+
+  res.json({ productId });
+});
