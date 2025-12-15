1f755b47f8980518e5020c5c3c99b4031ed75479
Scurhly <rafaelong200208@gmail.com>
2025-12-15 18:10:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -18,5 +18,33 @@ app.post("/test", (req, res) => {
 
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on https://dev-product-test-production.up.railway.app:${PORT}`);
 });
+
+app.post("/create-product", async (req, res) => {
+    const { amount, userId } = req.body;
+  
+    if (!Number.isInteger(amount) || amount < 1 || amount > 20000) {
+      return res.status(400).json({ error: "Invalid amount" });
+    }
+  
+ 
+    const PRODUCT_MAP = {
+      1: 3480326033,
+      5: 3480326507,
+      10: 3480326513,
+      50: 3480326517,
+      100: 3480327484,
+      1000: 3480327494,
+      2000: 3480327507,
+      5000: 3480327517,
+    };
+  
+    const productId = PRODUCT_MAP[amount];
+    if (!productId) {
+      return res.status(400).json({ error: "Unsupported amount" });
+    }
+  
+    res.json({ productId });
+  });
+  
